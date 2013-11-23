1fbae9abd73ed3a2c90644292ff6ca53c06e809d
Sundar Raman <cybertoast@gmail.com>
2013-11-22 19:47:32 -0500

    Test cases

--- test_router.py
+++ test_router.py
@@ -11,20 +11,33 @@ if sys.version_info[0] < 3:
 else:
     b = lambda s: s.encode('utf-8')
 
-# Define a few dummy routes
-routes = [ 
-     # u'route', 'controller.method', {options:value})
-     ('/', 'show', {'methods':['GET']}),
-     ('/new', 'create', {'methods':['POST']}),
-     ('/update', 'update', {'methods':['PUT']})
+# The main functions have to be defined outside 
+#   the RouterTestCase class, since their context
+#   would not be visible otherwise
+def show():
+    return 'show'
 
-     # We'll define the DELETE method in the view function decorator
-     # ('/', 'delete', {'methods':['DELETE']})
-]
+def create():
+    return 'create'
+
+def update():
+    return 'update'
+
 
 class RouterTestCase(unittest.TestCase):
 
     def setUp(self):
+
+        routes = [ 
+            # u'route', 'controller.method', {options:value})
+            ('/', 'show', {'methods':['GET']}),
+            ('/new', 'create', {'methods':['POST']}),
+            ('/update', 'update', {'methods':['PUT']})
+
+            # We'll define the DELETE method in the view function decorator
+            # ('/', 'delete', {'methods':['DELETE']})
+        ]
+
         app = Flask(__name__)
         app.debug = True
         app.secret_key = '1234'
@@ -32,26 +45,33 @@ class RouterTestCase(unittest.TestCase):
 
         Router(app, routes)
 
-        def show():
-            return 'show'
-
-        def create():
-            return 'create'
-
-        def update():
-            return 'update'
-
         @app.route('/delete', methods=['DELETE'])
         def delete():
             return 'delete'
 
-    def test_ensure_routes(self):
-        pass
-
-    def test_https_good_referer(self):
+    def test_retrieve(self):
         with self.app.test_client() as client:
             rv  = client.get('/')
-            self.assertEqual(rv.content, 'show')
+            self.assertEqual(rv.status_code, 200)
+            self.assertEqual(rv.data, 'show')
+
+    def test_create(self):
+        with self.app.test_client() as client:
+            rv  = client.post('/new')
+            self.assertEqual(rv.status_code, 200)
+            self.assertEqual(rv.data, 'create')
+
+    def test_update(self):
+        with self.app.test_client() as client:
+            rv  = client.put('/update')
+            self.assertEqual(rv.status_code, 200)
+            self.assertEqual(rv.data, 'update')
+
+    def test_update(self):
+        with self.app.test_client() as client:
+            rv  = client.delete('/delete')
+            self.assertEqual(rv.status_code, 200)
+            self.assertEqual(rv.data, 'delete')
 
 def suite():
     suite = unittest.TestSuite()
